09212a2fd30b36c7e087cfcd28b818c51f5d6d38
NexusDevelopmentUA <unnexusgcm@gmail.com>
2016-11-04 16:57:00 +0200

    Fixed IDE0006 issue(or part of it).

--- Airsoft_registrator/Activities/DialogSignIn.cs
+++ Airsoft_registrator/Activities/DialogSignIn.cs
@@ -21,7 +21,6 @@ namespace Airsoft_registrator.Activities
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Create your application here
         }
     }
